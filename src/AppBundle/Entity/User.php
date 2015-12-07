d6b4929a95e4b38898e4ca53f81d3eb3852f9296
Mathieu Piot <mathieu.piot@grignon.inra.fr>
2015-12-07 15:51:19 +0100

    Ajout des attributs: firstName, lastName, company à l'objet User, et adaptation des formulaire RegistrationType et ProfileType.

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -19,9 +20,92 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(name="first_name", type="string", length=255)
+     * @Assert\NotBlank(message="Please enter your first name.", groups={"Registration", "Profile"})
+     */
+    protected $firstName;
+
+    /**
+     * @ORM\Column(name="last_name", type="string", length=255)
+     * @Assert\NotBlank(message="Please enter your last name.", groups={"Registration", "Profile"})
+     */
+    protected $lastName;
+
+    /**
+     * @ORM\Column(name="company", type="string", length=255)
+     * @Assert\NotBlank(message="Please enter your company's name.", groups={"Registration", "Profile"})
+     */
+    protected $company;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName) {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName() {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastNAme
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+
+    public function setLastName($lastName) {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName() {
+        return $this->lastName;
+    }
+
+    /**
+     * Set company
+     *
+     * @param string $company
+     *
+     * @return User
+     */
+
+    public function setCompany($company) {
+        $this->company = $company;
+        return $this;
+    }
+
+    /**
+     * Get company
+     *
+     * @return string
+     */
+    public function getCompany() {
+        return $this->company;
     }
 }
---
+++ src/AppBundle/Form/ProfileType.php
@@ -0,0 +1,28 @@
+<?php
+// src/AppBundle/Form/ProfileType.php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstName');
+        $builder->add('lastName');
+        $builder->add('company');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_profile';
+    }
+}
+
