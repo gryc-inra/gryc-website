39600448c03269368513f1cfbdf324e98c56e424
Mathieu Piot <mathieu.piot@grignon.inra.fr>
2015-12-10 09:30:46 +0100

    Création de DataFixtures pour créer des Users et des catégories pour le formulaire de contact.

---
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -0,0 +1,72 @@
+<?php
+// src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\ContactUsCategory;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+class LoadModule extends AbstractFixture implements ContainerAwareInterface
+{
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager) {
+
+        //---------//
+        //  Users  //
+        //---------//
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        // Create a user Mathieu
+        $user = $userManager->createUser();
+        $user->setEmail('mathieu.piot@agroparistech.fr');
+        $user->setUsername('Mathieu');
+        $user->setPlainPassword('mathieu');
+        $user->setFirstName('Mathieu');
+        $user->setLastName('Piot');
+        $user->setCompany('INRA');
+        $user->setEnabled(true);
+        $user->addRole('ROLE_ADMIN');
+
+        // Create a user Hugo
+        $user2 = $userManager->createUser();
+        $user2->setEmail('hugo.devillers@grignon.inra.fr');
+        $user2->setUsername('Hugo');
+        $user2->setPlainPassword('hugo');
+        $user2->setFirstName('Hugo');
+        $user2->setLastName('Devillers');
+        $user2->setCompany('INRA');
+        $user2->setEnabled(true);
+
+        $manager->persist($user);
+        $manager->persist($user2);
+
+        $this->setReference('user-mathieu', $user);
+        $this->setReference('user-hugo', $user2);
+
+        //------------------------//
+        //  ContactUs Categories  //
+        //------------------------//
+
+        $categoriesName = array('Account question', 'Bug signalement', 'Ask informations');
+
+        foreach ($categoriesName as $categoryName) {
+            $category = new ContactUsCategory();
+            $category->setName($categoryName);
+
+            $manager->persist($category);
+
+            $this->setReference('category-'.$categoryName, $category);
+        }
+
+        $manager->flush();
+    }
+}
