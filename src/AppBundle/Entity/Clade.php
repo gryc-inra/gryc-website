21122605632d0ad6a32c124a79259cabc262fb8c
Mathieu Piot <mathieu.piot@grignon.inra.fr>
2016-02-05 16:03:53 +0100

    Create Clade and Species entity/repository

---
+++ src/AppBundle/Entity/Clade.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Clade
+ *
+ * @ORM\Table(name="clade")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CladeRepository")
+ */
+class Clade
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="mainClade", type="boolean")
+     */
+    private $mainClade;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Clade
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Clade
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set mainClade
+     *
+     * @param boolean $mainClade
+     *
+     * @return Clade
+     */
+    public function setMainClade($mainClade)
+    {
+        $this->mainClade = $mainClade;
+
+        return $this;
+    }
+
+    /**
+     * Get mainClade
+     *
+     * @return bool
+     */
+    public function getMainClade()
+    {
+        return $this->mainClade;
+    }
+
+    public function isMainCladeToString()
+    {
+        if ($this->mainClade) {
+            return 'Yes';
+        } else {
+            return 'No';
+        }
+    }
+}
+
---
+++ src/AppBundle/Entity/Species.php
@@ -0,0 +1,344 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Species
+ *
+ * @ORM\Table(name="species")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SpeciesRepository")
+ */
+class Species
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Clade")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $clade;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="scientificName", type="string", length=255, unique=true)
+     */
+    private $scientificName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="species", type="string", length=255, unique=true)
+     */
+    private $species;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="genus", type="string", length=255)
+     */
+    private $genus;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lineage", type="string", length=255)
+     */
+    private $lineage;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="taxid", type="integer", nullable=true, unique=true)
+     */
+    private $taxid;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="geneticCode", type="integer")
+     */
+    private $geneticCode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mitoCode", type="integer", length=255, unique=true)
+     */
+    private $mitoCode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="synonymes", type="string", nullable=true)
+     */
+    private $synonymes;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set clade
+     *
+     * @param Clade $clade
+     *
+     * @return Species
+     */
+    public function setClade(Clade $clade)
+    {
+        $this->clade = $clade;
+
+        return $this;
+    }
+
+    /**
+     * Get clade
+     *
+     * @return Clade
+     */
+    public function getClade()
+    {
+        return $this->clade;
+    }
+
+    /**
+     * Set scientificName
+     *
+     * @param string $scientificName
+     *
+     * @return Species
+     */
+    public function setScientificName($scientificName)
+    {
+        $this->scientificName = $scientificName;
+
+        return $this;
+    }
+
+    /**
+     * Get scientificName
+     *
+     * @return string
+     */
+    public function getScientificName()
+    {
+        return $this->scientificName;
+    }
+
+    /**
+     * Set species
+     *
+     * @param string $species
+     *
+     * @return Species
+     */
+    public function setSpecies($species)
+    {
+        $this->species = $species;
+
+        return $this;
+    }
+
+    /**
+     * Get species
+     *
+     * @return string
+     */
+    public function getSpecies()
+    {
+        return $this->species;
+    }
+
+    /**
+     * Set genus
+     *
+     * @param string $genus
+     *
+     * @return Species
+     */
+    public function setGenus($genus)
+    {
+        $this->genus = $genus;
+
+        return $this;
+    }
+
+    /**
+     * Get genus
+     *
+     * @return string
+     */
+    public function getGenus()
+    {
+        return $this->genus;
+    }
+
+    /**
+     * Set lineage
+     *
+     * @param string $lineage
+     *
+     * @return Species
+     */
+    public function setLineage($lineage)
+    {
+        $this->lineage = $lineage;
+
+        return $this;
+    }
+
+    /**
+     * Get lineage
+     *
+     * @return string
+     */
+    public function getLineage()
+    {
+        return $this->lineage;
+    }
+
+    /**
+     * Set taxid
+     *
+     * @param integer $taxid
+     *
+     * @return Species
+     */
+    public function setTaxid($taxid)
+    {
+        $this->taxid = $taxid;
+
+        return $this;
+    }
+
+    /**
+     * Get taxid
+     *
+     * @return int
+     */
+    public function getTaxid()
+    {
+        return $this->taxid;
+    }
+
+    /**
+     * Set geneticCode
+     *
+     * @param integer $geneticCode
+     *
+     * @return Species
+     */
+    public function setGeneticCode($geneticCode)
+    {
+        $this->geneticCode = $geneticCode;
+
+        return $this;
+    }
+
+    /**
+     * Get geneticCode
+     *
+     * @return int
+     */
+    public function getGeneticCode()
+    {
+        return $this->geneticCode;
+    }
+
+    /**
+     * Set mitoCode
+     *
+     * @param string $mitoCode
+     *
+     * @return Species
+     */
+    public function setMitoCode($mitoCode)
+    {
+        $this->mitoCode = $mitoCode;
+
+        return $this;
+    }
+
+    /**
+     * Get mitoCode
+     *
+     * @return integer
+     */
+    public function getMitoCode()
+    {
+        return $this->mitoCode;
+    }
+
+    /**
+     * Set synonymes
+     *
+     * @param string $synonymes
+     *
+     * @return Species
+     */
+    public function setSynonymes($synonymes)
+    {
+        $this->synonymes = $synonymes;
+
+        return $this;
+    }
+
+    /**
+     * Get synonymes
+     *
+     * @return string
+     */
+    public function getSynonymes()
+    {
+        return $this->synonymes;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Species
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
+
