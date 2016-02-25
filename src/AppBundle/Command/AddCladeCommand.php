110053bb464c568c0d1741628897b18cf5f3813d
Mathieu Piot <mathieu.piot@grignon.inra.fr>
2016-02-25 13:37:08 +0100

    Add some controls and reorganize the code of Add and Edit Clade command
    Use the 3 methods: initialize, interact and execute in thos class.
    Verify more informations like the name of the clade isn't already used, control on the arguments give in the command line

--- src/AppBundle/Command/AddCladeCommand.php
+++ src/AppBundle/Command/AddCladeCommand.php
@@ -5,89 +5,154 @@ namespace AppBundle\Command;
 
 use AppBundle\Entity\Clade;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class AddCladeCommand extends ContainerAwareCommand
 {
+    // This attribute contains all the clades persisted in the database
+    /**
+     * @var array Clade
+     */
+    private $clades;
+
     protected function configure()
     {
         $this
             ->setName('bio:clade:add')
+            ->setAliases(array('bio:add:clade'))
             ->setDescription('Add a clade')
-        ;
+            ->setDefinition(array(
+                new InputArgument('name', InputArgument::REQUIRED, 'The name of the clade'),
+                new InputArgument('description', InputArgument::REQUIRED, 'The description of the clade'),
+                new InputOption('main-clade', false, InputOption::VALUE_NONE, 'Set the clade as a main clade'),
+            ))
+            ->setHelp(<<<EOT
+The <info>bio:clade:add</info> command creates a clade:
+  <info>bin/console bio:clade:add Candida</info>
+This interactive shell will ask you for a description.
+You can alternatively specify the description as the second argument:
+  <info>bin/console bio:clade:add Candida "Candida, a so beautiful clade."</info>
+You can create a main clade via the main-clade flag:
+  <info>bin/console bio:clade:add Candida --main-clade</info>
+
+EOT
+            );
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    // Here, prepare some variables used later and some control on the command line argument
+    protected function initialize(InputInterface $input, OutputInterface $output)
     {
-        // Créer le helper
-        $helper = $this->getHelper('question');
+        parent::initialize($input, $output);
 
+        // Retrieve the list of all clades, and put it in a array with the clade's name as key and the clade's object as value
+        $clades = $this->getContainer()->get('doctrine')->getManager()->getRepository('AppBundle:Clade')->findAll();
+        foreach ($clades as $clade) {
+            $this->clades[$clade->getName()] = $clade;
+        }
+
+        // If the user give a name to the new clade in the command line, verify the 'pattern' of the name and that no clade with the same name doesn't already exists
+        if ($answer = $input->getArgument('name')) {
+            if (!preg_match('#[A-Z][a-z]*$#', $answer)) {
+                throw new \RuntimeException(
+                    'The name have not the goot pattern ! (eg: "Candida")'
+                );
+            }
+
+            if (array_key_exists($answer, $this->clades)) {
+                throw new \RuntimeException(
+                    'This clade already exists !'
+                );
+            }
+        }
+    }
+
+    // Here we prepare the question to the user, if he doesn't give all the arguments in the command line
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
         do {
-            // On crée l'objet à remplir
-            $clade = new Clade();
+            $questions = array();
 
-            // On demande à l'utilisateur des informations pour hydrater l'objet
+            if (!$input->getArgument('name')) {
+                $question = new Question('Please enter the name of the clade: ');
+                // In this validator: control the 'pattern' and that no clade with this name doesn't already exists
+                $question->setValidator(function ($answer) {
+                    if (!preg_match('#[A-Z][a-z]*$#', $answer)) {
+                        throw new \RuntimeException(
+                            'The name have not the goot pattern ! (eg: "Candida")'
+                        );
+                    }
 
-            // CladeName
-            $cladeNameQuestion = new Question("\nPlease enter the name of the clade:\n");
-            $cladeNameQuestion->setValidator(function ($answer) {
-                if (!preg_match('#[A-Z][a-z]*$#', $answer)) {
-                    throw new \RuntimeException(
-                        'The name have not the goot pattern ! (eg: "Candida")'
-                    );
-                }
+                    if (array_key_exists($answer, $this->clades)) {
+                        throw new \RuntimeException(
+                            'This clade already exist.'
+                        );
+                    }
 
-                return $answer;
-            });
-            $clade->setName($helper->ask($input, $output, $cladeNameQuestion));
+                    return $answer;
+                });
+                $questions['name'] = $question;
+            }
 
-            // CladeDescription
-            $cladeDescriptionQuestion = new Question("\nPlease enter the description of the clade:\n");
-            $cladeDescriptionQuestion->setValidator(function ($answer) {
-                if (empty($answer)) {
-                    throw new \RuntimeException(
-                        'The description can\'t be empty !'
-                    );
-                }
+            if (!$input->getArgument('description')) {
+                $question = new Question('Please enter the description of the clade: ');
+                // The description can't be empty
+                $question->setValidator(function ($answer) {
+                    if (empty($answer)) {
+                        throw new \RuntimeException(
+                            'The description can\'t be empty !'
+                        );
+                    }
 
-                return $answer;
-            });
-            $clade->setDescription($helper->ask($input, $output, $cladeDescriptionQuestion));
+                    return $answer;
+                });
+                $questions['description'] = $question;
+            }
 
-            // MainClade
-            $isMainCladeQuestion = new ConfirmationQuestion("\nIs it a Main Clade ? (y/N)\n", false);
-            $clade->setMainClade($helper->ask($input, $output, $isMainCladeQuestion));
+            // We do a loop on on the questions, and ask it to the user
+            foreach ($questions as $name => $question) {
+                $answer = $this->getHelper('question')->ask($input, $output, $question);
+                $input->setArgument($name, $answer);
+            }
 
-            // On fait un résumé des données récupérées
-            $userData = "Clade name:\t".$clade->getName()."\n";
-            $userData .= ($clade->getMainClade()) ? "Main clade:\tYes\n" : "Main clade:\tNo\n";
-            $userData .= "Description:\n".$clade->getDescription()."\n";
+            // Ask to the user if he is sure of his answers.
+            $output->writeln(array(
+                '',
+                'Summary:',
+                'Name: '.$input->getArgument('name'),
+                'Description: '.$input->getArgument('description'),
+            ));
+            $output->writeln('Main clade: '.(($input->getOption('main-clade')) ? 'Yes' : 'No'));
 
-            $output->writeln("\n".$userData);
+            $confirmQuestion = new ConfirmationQuestion('<question>Is it correct ? (y/N)</question> ', false);
 
-            // On demande à l'utilisateur si les données sont bonnes ou pas
-            $confirmQuestion = new ConfirmationQuestion("Is it correct ? (y/N)\n", false);
-            // Si les données ne sont pas bonnes, on recommence la boucle
-            // On supprime les arguments, pour poser la question à l'utilisateur
-            // On détruit l'objet Clade
-            if (!$helper->ask($input, $output, $confirmQuestion)) {
+            if (!$this->getHelper('question')->ask($input, $output, $confirmQuestion)) {
                 $confirm = false;
                 $input->setArgument('name', null);
                 $input->setArgument('description', null);
-                unset($clade);
             } else {
                 $confirm = true;
             }
         } while (!$confirm);
+    }
 
-        // On persiste l'objet
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Create the Clade object and set all the arguments
+        $clade = new Clade();
+        $clade->setName($input->getArgument('name'));
+        $clade->setDescription($input->getArgument('description'));
+        $clade->setMainClade($input->getOption('main-clade'));
+
+        // Persist and flush the object in the database
         $em = $this->getContainer()->get('doctrine')->getManager();
         $em->persist($clade);
         $em->flush();
 
-        $output->writeln('<info>The clade was successfully added</info>');
+        $output->writeln('<info>The clade was successfully added.</info>');
     }
 }
--- src/AppBundle/Command/EditCladeCommand.php
+++ src/AppBundle/Command/EditCladeCommand.php
@@ -8,104 +8,186 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class EditCladeCommand extends ContainerAwareCommand
 {
+    // Those attributes contain all the clade in the database and the clade before edition
+    /**
+     * @var array Clade
+     */
+    private $cladeList;
+
+    /**
+     * @var Clade
+     */
+    private $clade;
+
     protected function configure()
     {
         $this
             ->setName('bio:clade:edit')
+            ->setAliases(array('bio:edit:clade'))
             ->setDescription('Edit a clade')
-            ->addArgument(
-                'cladeId',
-                InputArgument::OPTIONAL,
-                'Id of the clade'
-            )
-        ;
+            ->setDefinition(array(
+                new InputArgument('clade', InputArgument::REQUIRED, 'Id of the clade'),
+                new InputArgument('name', InputArgument::REQUIRED, 'The name of the clade'),
+                new InputArgument('description', InputArgument::REQUIRED, 'The description of the clade'),
+                new InputOption('main-clade', false, InputOption::VALUE_NONE, 'Set the clade as a main clade'),
+            ))
+            ->setHelp(<<<EOT
+The <info>bio:clade:edit</info> command edit a clade:
+  <info>bin/console bio:clade:edit Candida</info>
+This interactive shell will ask you for a name and description.
+You can alternatively specify the name and description as the second and third arguments:
+  <info>bin/console bio:clade:edit Candida CandidaII "Candida, a so beautiful clade."</info>
+You can create a main clade via the main-clade flag:
+  <info>bin/console bio:clade:edit Candida --main-clade</info>
+
+EOT
+            );
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    // Here, prepare some variables used later and some control on the command line argument
+    protected function initialize(InputInterface $input, OutputInterface $output)
     {
-        $helper = $this->getHelper('question');
-        $em = $this->getContainer()->get('doctrine')->getManager();
+        parent::initialize($input, $output);
 
-        // Si l'utilisateur ne donne pas d'ID lors de la demande d'édition du clade, on lui affiche la liste des clades
-        // et il choisi le clade qu'il souhaite modifier
-        if (!$input->getArgument('cladeId')) {
-            // Appeller la commande bio:clade:list, pour donner à l'utilisateur une liste des clades
-            $listCladeCommand = $this->getApplication()->find('bio:clade:list');
-            $listCladeCommandInput = new ArrayInput(array('command' => 'bio:clade:list'));
-            $listCladeCommand->run($listCladeCommandInput, $output);
-
-            // On demande à l'utilisateur quel clade il veut modifier
-            $cladeIdQuestion = new Question("\nPlease enter the ID of the clade:\n");
-            // On récupère le clade
-            $input->setArgument('cladeId', $helper->ask($input, $output, $cladeIdQuestion));
+        // Retrieve all clades persisted in the database and put it in the cladeList attribute
+        $clades = $this->getContainer()->get('doctrine')->getManager()->getRepository('AppBundle:Clade')->findAll();
+        foreach ($clades as $clade) {
+            $this->cladeList[$clade->getName()] = $clade;
         }
 
-        $clade = $em->getRepository('AppBundle:Clade')->findOneById($input->getArgument('cladeId'));
+        // If the user give wich clade he wants edits, we control that Clade exists, and if it exists put it in the argument
+        if ($input->getArgument('clade')) {
+            if (!in_array($input->getArgument('clade'), array_keys($this->cladeList))) {
+                throw new \RuntimeException(
+                    'The clade doesn\'t exist !'
+                );
+            }
+            $input->setArgument('clade', $this->cladeList[$input->getArgument('clade')]);
+        }
 
+        // If the user give a new name in the command line, control that name doesn't already exist
+        if (array_key_exists($input->getArgument('name'), $this->cladeList) and $input->getArgument('name') !== $input->getArgument('clade')->getName()) {
+            throw new \RuntimeException(
+                'This clade already exists !'
+            );
+        }
+    }
+
+    // Here we prepare the question to the user, if he doesn't give all the arguments in the command line
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
         do {
-            // On propose à l'utilisateur de modifier les champs
+            $questions = array();
 
-            // CladeName
-            $cladeNameQuestion = new Question("\nPlease enter the name of the clade: (actual value: ".$clade->getName().")\n", $clade->getName());
-            $cladeNameQuestion->setValidator(function ($answer) {
-                if (!preg_match('#[A-Z][a-z]*$#', $answer)) {
-                    throw new \RuntimeException(
-                        'The name have not the goot pattern ! (eg: "Candida")'
-                    );
-                }
+            // If the user doesn't give the clade he wants edit, we ask him
+            if (!$input->getArgument('clade')) {
+                // List all the persisted clades, call the bio:clade:list command to do it
+                $listCladeCommand = $this->getApplication()->find('bio:clade:list');
+                $listCladeCommandInput = new ArrayInput(array('command' => 'bio:clade:list'));
+                $listCladeCommand->run($listCladeCommandInput, $output);
 
-                return $answer;
-            });
-            $cladeName = $helper->ask($input, $output, $cladeNameQuestion);
+                $question = new Question('Enter the name of the clade: ');
+                $question->setAutocompleterValues(array_keys($this->cladeList));
+                // Verify that the name of the clade is an existing clade, if yes return the clade object
+                $question->setValidator(function ($answer) {
+                    if (!in_array($answer, array_keys($this->cladeList))) {
+                        throw new \RuntimeException(
+                            'The clade doesn\'t exist !'
+                        );
+                    }
 
-            // CladeDescription
-            $cladeDescriptionQuestion = new Question("\nPlease enter the description of the clade: (actual value: ".$clade->getDescription().")\n", $clade->getDescription());
-            $cladeDescriptionQuestion->setValidator(function ($answer) {
-                if (empty($answer)) {
-                    throw new \RuntimeException(
-                        'The description can\'t be empty !'
-                    );
-                }
+                    return $this->cladeList[$answer];
+                });
 
-                return $answer;
-            });
-            $cladeDescription = $helper->ask($input, $output, $cladeDescriptionQuestion);
+                // We need to ask this question before ask other questions, because we use the data of the editing clade in the questions
+                $answer = $this->getHelper('question')->ask($input, $output, $question);
+                $input->setArgument('clade', $answer);
+            }
 
-            // MainClade
-            $isMainCladeQuestion = new ConfirmationQuestion("\nIs it a Main Clade ? (y/n) (actual value: ".(($clade->getMainClade()) ? 'y' : 'n').")\n", $clade->getMainClade());
-            $cladeMainClade = $helper->ask($input, $output, $isMainCladeQuestion);
+            // Definy the clade attribut: Here we are sure to can it:
+            // - if the user give it in the command line, we have set the attribut in the initialize method
+            // - if the user doesn't give the argument in the command line, we have ask him which clade he wants editing previously
+            $this->clade = $input->getArgument('clade');
 
-            // On fait un résumé des données récupérées
-            $userData = "Clade name:\t".$cladeName."\n";
-            $userData .= ($cladeMainClade) ? "Main clade:\tYes\n" : "Main clade:\tNo\n";
-            $userData .= "Description:\n".$cladeDescription."\n";
+            if (!$input->getArgument('name')) {
+                $question = new Question("\nPlease enter the name of the clade: (actual value: ".$this->clade->getName().")\n", $this->clade->getName());
+                // Control that name have a good pattern and doesn't already exist
+                $question->setValidator(function ($answer) use ($input) {
+                    if (!preg_match('#[A-Z][a-z]*$#', $answer)) {
+                        throw new \RuntimeException(
+                            'The name have not the goot pattern ! (eg: "Candida")'
+                        );
+                    }
 
-            $output->writeln("\n".$userData);
+                    if (array_key_exists($answer, $this->cladeList) and $answer !== $input->getArgument('clade')->getName()) {
+                        throw new \RuntimeException(
+                            'This clade already exists !'
+                        );
+                    }
 
-            // On demande à l'utilisateur si les données sont bonnes ou pas
-            $confirmQuestion = new ConfirmationQuestion("Is it correct ? (y/N)\n", false);
+                    return $answer;
+                });
+                $questions['name'] = $question;
+            }
 
-            // Si les données ne sont pas bonnes, on recommence la boucle
-            if (!$helper->ask($input, $output, $confirmQuestion)) {
+            if (!$input->getArgument('description')) {
+                $question = new Question("\nPlease enter the description of the clade: (actual value: ".$this->clade->getDescription().")\n", $this->clade->getDescription());
+                // Just verify the description isn't empty
+                $question->setValidator(function ($answer) {
+                    if (empty($answer)) {
+                        throw new \RuntimeException(
+                            'The description can\'t be empty !'
+                        );
+                    }
+
+                    return $answer;
+                });
+                $questions['description'] = $question;
+            }
+
+            // Loop on the questions
+            foreach ($questions as $name => $question) {
+                $answer = $this->getHelper('question')->ask($input, $output, $question);
+                $input->setArgument($name, $answer);
+            }
+
+            // Ask to the user if he is sure of his answers.
+            $output->writeln(array(
+                '',
+                'Summary:',
+                'Name: '.$input->getArgument('name'),
+                'Description: '.$input->getArgument('description'),
+            ));
+            $output->writeln('Main clade: '.(($input->getOption('main-clade')) ? 'Yes' : 'No'));
+
+            $confirmQuestion = new ConfirmationQuestion('<question>Is it correct ? (y/N)</question> ', false);
+
+            if (!$this->getHelper('question')->ask($input, $output, $confirmQuestion)) {
                 $confirm = false;
+                $input->setArgument('name', null);
+                $input->setArgument('description', null);
             } else {
                 $confirm = true;
             }
         } while (!$confirm);
+    }
 
-        // On associe les nouvelles valeurs à l'objet
-        $clade->setName($cladeName);
-        $clade->setDescription($cladeDescription);
-        $clade->setMainClade($cladeMainClade);
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Edit the clade object
+        $this->clade->setName($input->getArgument('name'));
+        $this->clade->setDescription($input->getArgument('description'));
+        $this->clade->setMainClade($input->getOption('main-clade'));
 
-        // On persiste l'objet
-        $em->flush();
+        // Persist it
+        $this->getContainer()->get('doctrine')->getManager()->flush();
 
         $output->writeln('<info>The clade was successfully edited</info>');
     }
