8dd6dbd006fe43b85d88a1bf24360c24bae8b0fc
Mathieu Piot <mathieu.piot@grignon.inra.fr>
2016-03-29 09:56:26 +0200

    Add roles management in admin

--- src/AppBundle/Controller/AdminUserController.php
+++ src/AppBundle/Controller/AdminUserController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
 use AppBundle\Form\Type\AdminUserRightsType;
+use AppBundle\Form\Type\AdminUserRolesType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -47,6 +48,31 @@ class AdminUserController extends Controller
         }
 
         return $this->render('admin/user/edit-rights.html.twig', array(
+            'user' => $user,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/edit-roles/{usernameCanonical}", name="user_roles_edit")
+     */
+    public function editRolesAction(Request $request, User $user)
+    {
+        $form = $this->createForm(AdminUserRolesType::class, $user);
+        $form->add('save', SubmitType::class);
+        
+        $form->handleRequest($request);
+        
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('success', 'The user roles were edited !');
+
+            return $this->redirectToRoute('user_list');
+        }
+        
+        return $this->render('admin/user/edit-roles.html.twig', array(
             'user' => $user,
             'form' => $form->createView(),
         ));
---
+++ src/AppBundle/Form/Type/AdminUserRolesType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use AppBundle\Repository\StrainRepository;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminUserRolesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('roles', ChoiceType::class, array(
+                'choices' => array(
+                    'User' => 'ROLE_USER',
+                    'Administrator' => 'ROLE_ADMIN',
+                ),
+                'multiple' => true,
+                'expanded' => true,
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User',
+        ));
+    }
+}
