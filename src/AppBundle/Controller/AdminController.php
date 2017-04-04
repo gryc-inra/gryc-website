931fd2881e3780a3410115d7c38e41d4988a11fd
Mathieu Piot <mathieu.piot@inra.fr>
2017-04-04 07:16:25 +0200

    Adapt AdminController

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -1,25 +1,14 @@
 <?php
 
-// src/AppBundle/Controller/AdminController.php
-
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-/**
- * Administration panel controller.
- *
- * @Route("admin")
- */
 class AdminController extends Controller
 {
     /**
-     * Administration panel index page.
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @Route("/", name="admin_index")
+     * @Route("/admin", name="admin_index")
      */
     public function indexAction()
     {
