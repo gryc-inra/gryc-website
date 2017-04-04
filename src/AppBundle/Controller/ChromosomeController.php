0c4246181decbca13140d62efd0bf2cfbca02eb2
Mathieu Piot <mathieu.piot@inra.fr>
2017-04-04 07:17:55 +0200

    Adapt ChromosomeController

--- src/AppBundle/Controller/ChromosomeController.php
+++ src/AppBundle/Controller/ChromosomeController.php
@@ -1,7 +1,5 @@
 <?php
 
-// src/AppBundle/Controller/ChromosomeController.php
-
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Chromosome;
@@ -10,21 +8,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-/**
- * Chromosome controller.
- *
- * @Route("chromosome")
- */
 class ChromosomeController extends Controller
 {
     /**
-     * Chromosome view.
-     *
-     * @param Chromosome $chromosome
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @Route("/{slug}", name="chromosome_view")
+     * @Route("/chromosome/{slug}", name="chromosome_view")
      * @ParamConverter("chromosome", class="AppBundle:Chromosome", options={
      *     "repository_method" = "getChromosomeWithStrainAndSpecies",
      *     "mapping": {"slug": "slug"},
