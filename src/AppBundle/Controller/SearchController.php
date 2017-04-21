0acaa1c1f06028d88596fedacf1af780bf7bbc91
Mathieu Piot <mathieu.piot@inra.fr>
2017-04-21 14:55:47 +0200

    In ES, store only locus: features and products as nested objects.

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\SearchRepository\GlobalRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,19 +20,11 @@ class SearchController extends Controller
     {
         $keyword = null !== $request->get('q') ? $request->get('q') : '';
 
-        // Get the query
-        $repository = new GlobalRepository();
-        $query = $repository->searchQuery($keyword, $this->getUser());
+        $repositoryManager = $this->get('fos_elastica.manager');
+        $repository = $repositoryManager->getRepository('AppBundle:Locus');
+        $results = $repository->findByNameNoteAnnotation($keyword, $this->getUser());
 
-        // Execute the query
-        $mngr = $this->get('fos_elastica.index_manager');
-        $search = $mngr->getIndex('app')->createSearch();
-        $search->addType('locus');
-        $search->addType('feature');
-        $search->addType('product');
-        $resultSet = $search->search($query, self::HITS_PER_PAGE);
-        $transformer = $this->get('fos_elastica.elastica_to_model_transformer.collection.app');
-        $results = $transformer->transform($resultSet->getResults());
+        dump($results);
 
         // Return the view
         return $this->render('search\quickSearch.html.twig', [
---
+++ src/AppBundle/SearchRepository/LocusRepository.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace AppBundle\SearchRepository;
+
+use AppBundle\Entity\User;
+use Elastica\Query;
+use Elastica\Query\BoolQuery;
+use Elastica\Query\Match;
+use Elastica\Query\MultiMatch;
+use Elastica\Query\Nested;
+use Elastica\Query\Term;
+use FOS\ElasticaBundle\Repository;
+
+class LocusRepository extends Repository
+{
+    public function findByNameNoteAnnotation($keyword = null, User $user = null)
+    {
+        // Create the Query
+        $query = new BoolQuery();
+        $query->setMinimumShouldMatch(1);
+
+        // LOCUS
+        $locusQuery = new BoolQuery();
+        $locusQuery->setMinimumShouldMatch(1);
+        $query->addShould($locusQuery);
+
+        $locusNameQuery = new MultiMatch();
+        $locusNameQuery->setQuery($keyword);
+        $locusNameQuery->setFields(['name^2', 'name.ngramed']);
+        $locusQuery->addShould($locusNameQuery);
+
+        $locusNoteQuery = new MultiMatch();
+        $locusNoteQuery->setFields(['note^2', 'note.stemmed']);
+        $locusNoteQuery->setFuzziness('AUTO');
+        $locusNoteQuery->setQuery($keyword);
+        $locusQuery->addShould($locusNoteQuery);
+
+        $locusGeneQuery = new Match();
+        $locusGeneQuery->setFieldQuery('annotation.gene', $keyword);
+        $locusQuery->addShould($locusGeneQuery);
+
+        // FEATURES
+        $featureNested = new Nested();
+        $featureNested->setPath('features');
+        $featureNested->setScoreMode('avg');
+        $query->addShould($featureNested);
+
+        $featureQuery = new BoolQuery();
+        $featureQuery->setMinimumShouldMatch(1);
+        $featureNested->setQuery($featureQuery);
+
+        $featureNameQuery = new MultiMatch();
+        $featureNameQuery->setQuery($keyword);
+        $featureNameQuery->setFields(['features.name^2', 'features.name.ngramed']);
+        $featureQuery->addShould($featureNameQuery);
+
+        $featureNoteQuery = new MultiMatch();
+        $featureNoteQuery->setFields(['features.note^2', 'features.note.stemmed']);
+        $featureNoteQuery->setFuzziness('AUTO');
+        $featureNoteQuery->setQuery($keyword);
+        $featureQuery->addShould($featureNoteQuery);
+
+        $featureGeneQuery = new Match();
+        $featureGeneQuery->setFieldQuery('features.annotation.gene', $keyword);
+        $locusQuery->addShould($featureGeneQuery);
+
+        // PRODUCTS
+        $featureProductNested = new Nested();
+        $featureProductNested->setPath('features');
+        $featureProductNested->setScoreMode('avg');
+        $query->addShould($featureProductNested);
+
+        $productNested = new Nested();
+        $productNested->setPath('features.products');
+        $productNested->setScoreMode('avg');
+        $featureProductNested->setQuery($productNested);
+
+        $productQuery = new BoolQuery();
+        $productQuery->setMinimumShouldMatch(1);
+        $productNested->setQuery($productQuery);
+
+        $productNameQuery = new MultiMatch();
+        $productNameQuery->setQuery($keyword);
+        $productNameQuery->setFields(['features.products.name^2', 'features.products.name.ngramed']);
+        $productQuery->addShould($productNameQuery);
+
+        $productNoteQuery = new MultiMatch();
+        $productNoteQuery->setFields(['features.products.note^2', 'features.products.note.stemmed']);
+        $productNoteQuery->setFuzziness('AUTO');
+        $productNoteQuery->setQuery($keyword);
+        $productQuery->addShould($productNoteQuery);
+
+        $productGeneQuery = new Match();
+        $productGeneQuery->setFieldQuery('features.products.annotation.gene', $keyword);
+        $productQuery->addShould($productGeneQuery);
+
+        // FILTER
+        // Create a BoolQuery with filters
+        $boolFilter = new BoolQuery();
+        $boolFilter->setMinimumShouldMatch(1);
+        $query->addFilter($boolFilter);
+
+        $userId = null !== $user ? $user->getId() : '';
+        // Set a user filter
+        $userFilter = new Term();
+        $userFilter->setTerm('authorized_users_id', $userId);
+        $boolFilter->addShould($userFilter);
+
+        // Set a public filter
+        $publicFilter = new Term();
+        $publicFilter->setTerm('public', true);
+        $boolFilter->addShould($publicFilter);
+
+        // Execute the query
+        return $this->find($query);
+    }
+}
