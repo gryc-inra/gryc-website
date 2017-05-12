0c652bc37e54a38cd814dab1b703ea00a9087f3d
Mathieu Piot <mathieu.piot@inra.fr>
2017-05-12 09:34:59 +0200

    Revert "Use CDN when possible"
    This reverts commit 4fb069aefa3a0bda8249936b4b5b34bdd0c03430.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,8 @@ module.exports = function(grunt) {
                 },
                 files: {
                     'web/css/app.min.css': [
+                        'bower_components/bootstrap/dist/css/bootstrap.min.css',
+                        'bower_components/fontawesome/css/font-awesome.min.css',
                         'bower_components/select2/dist/css/select2.min.css',
                         'bower_components/select2-bootstrap/dist/select2-bootstrap.min.css',
                         'app/Resources/public/css/*.css'
@@ -28,6 +30,8 @@ module.exports = function(grunt) {
             dist: {
                 files: {
                     'web/js/app.min.js':[
+                        'bower_components/jquery/dist/jquery.min.js',
+                        'bower_components/bootstrap/dist/js/bootstrap.min.js',
                         'bower_components/select2/dist/js/select2.min.js',
                         'app/Resources/public/js/*.js'
                     ]
@@ -43,11 +47,30 @@ module.exports = function(grunt) {
                 files: ['app/Resources/public/js/*.js'],
                 tasks: ['javascript']
             }
+        },
+        copy: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'bower_components/fontawesome/fonts',
+                        dest: 'web/fonts',
+                        src: ['**']
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/bootstrap/dist/fonts',
+                        dest: 'web/fonts',
+                        src: ['**']
+                    }
+                ]
+            }
         }
     });
 
     // Déclaration des différentes tâches
     grunt.registerTask('default', ['css','javascript']);
-    grunt.registerTask('css', ['cssmin']);
+    grunt.registerTask('css', ['cssmin', 'copy']);
     grunt.registerTask('javascript', ['uglify']);
+    grunt.registerTask('cp', ['copy']);
 };
