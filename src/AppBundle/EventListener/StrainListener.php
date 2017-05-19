43409d5b06c0de616e0835115bd0df7980825ac1
Mathieu Piot <mathieu.piot@inra.fr>
2017-05-19 12:12:11 +0200

    Test which fiel were edited in Strain listerner before reindex the hole genome

--- src/AppBundle/EventListener/StrainListener.php
+++ src/AppBundle/EventListener/StrainListener.php
@@ -3,7 +3,7 @@
 namespace AppBundle\EventListener;
 
 use AppBundle\Entity\Strain;
-use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use FOS\ElasticaBundle\Persister\ObjectPersister;
 
 class StrainListener
@@ -17,7 +17,7 @@ class StrainListener
         $this->memoryLimit = $memoryLimit;
     }
 
-    public function postUpdate(LifecycleEventArgs $args)
+    public function preUpdate(PreUpdateEventArgs $args)
     {
         $strain = $args->getEntity();
 
@@ -25,12 +25,37 @@ class StrainListener
             return;
         }
 
-        // Because we need to retrieve the complete Genome, we fix the memory limit on 512M
-        ini_set('memory_limit', $this->memoryLimit);
         $em = $args->getEntityManager();
-        $locusList = $em->getRepository('AppBundle:Locus')->findLocusFromStrain($strain);
+        $uow = $em->getUnitOfWork();
 
-        $this->objectPersister->replaceMany($locusList);
+        // Check if the public or authorizedUsers fields have been updated
+        // Per dafault, fix on false
+        $updateElasticsearch = false;
+
+        // If the public fields is updated: true
+        if (true === $args->hasChangedField('public')) {
+            $updateElasticsearch = true;
+        // For the authorizedUsers, it's mor complicated, because it's a ManyToMany relation
+        } else {
+            // Retrieve the collection
+            foreach ($uow->getScheduledCollectionUpdates() as $entityScheduledUpdates) {
+                // For each object in the collection, test the fieldName
+                // If one of them is authorizedUsers, at least on user have changed, set on true, and break the loop
+                if ('authorizedUsers' === $entityScheduledUpdates->getMapping()['fieldName']) {
+                    $updateElasticsearch = true;
+                    break;
+                }
+            }
+        }
+
+        // If we need to update the elasticsearch index, do it
+        if (true === $updateElasticsearch) {
+            // Because we need to retrieve the complete Genome, we fix the memory limit on 512M
+            ini_set('memory_limit', $this->memoryLimit);
+            $locusList = $em->getRepository('AppBundle:Locus')->findLocusFromStrain($strain);
+
+            $this->objectPersister->replaceMany($locusList);
+        }
 
         return;
     }
