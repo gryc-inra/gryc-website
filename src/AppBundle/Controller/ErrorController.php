e10bc4d653d200f7af0b2054da8eb445a4b6ac21
Mathieu Piot <mathieu.piot@inra.fr>
2017-06-02 15:09:17 +0200

    Add redirect for old URL, and set nginx 404 on the symfony 404 page

---
+++ src/AppBundle/Controller/ErrorController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Elastica\Exception\NotFoundException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Error controller.
+ */
+class ErrorController extends Controller
+{
+    /**
+     * @Route("/404", name="not_found_error")
+     */
+    public function notFoundAction()
+    {
+        throw $this->createNotFoundException();
+    }
+}
