60b50123375629a1e725e0fa1d43444063ba8965
Mathieu Piot <mathieu.piot@inra.fr>
2017-09-04 09:57:57 +0200

    Create a StrainsFilterType and use it in other forms

--- src/AppBundle/Form/Type/AdvancedSearchType.php
+++ src/AppBundle/Form/Type/AdvancedSearchType.php
@@ -2,14 +2,10 @@
 
 namespace AppBundle\Form\Type;
 
-use AppBundle\Entity\Strain;
-use Doctrine\ORM\EntityRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Validator\Constraints\Count;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class AdvancedSearchType extends AbstractType
@@ -31,54 +27,8 @@ class AdvancedSearchType extends AbstractType
                     new NotBlank(),
                 ],
             ])
-            ->add('strainsFilter', EntityType::class, [
-                'class' => 'AppBundle\Entity\Clade',
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('clade')
-                        ->leftJoin('clade.species', 'species')
-                            ->addSelect('species')
-                        ->leftJoin('species.strains', 'strain')
-                            ->addSelect('strain')
-                        ->leftJoin('strain.authorizedUsers', 'authorizedUsers')
-                        ->orderBy('clade.name', 'asc')
-                        ->where('strain.public = true')
-                        ->orWhere('authorizedUsers = :user')
-                        ->setParameter('user', $this->tokenStorage->getToken()->getUser());
-                },
-                'choice_value' => 'name',
-                'choice_label' => 'name',
-                'placeholder' => 'All',
-                'required' => false,
+            ->add('strainsFilter', StrainsFilterType::class, [
                 'mapped' => false,
-            ])
-            ->add('strains', EntityType::class, [
-                'class' => 'AppBundle\Entity\Strain',
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('strain')
-                        ->leftJoin('strain.species', 'species')
-                            ->addSelect('species')
-                        ->leftJoin('species.clade', 'clade')
-                            ->addSelect('clade')
-                        ->leftJoin('strain.authorizedUsers', 'authorizedUsers')
-                        ->orderBy('species.scientificName', 'asc')
-                        ->addOrderBy('strain.name', 'asc')
-                        ->where('strain.public = true')
-                        ->orWhere('authorizedUsers = :user')
-                            ->setParameter('user', $this->tokenStorage->getToken()->getUser());
-                },
-                'choice_label' => function (Strain $strain) {
-                    return $strain->getSpecies()->getScientificName().' ('.$strain->getName().')';
-                },
-                'choice_attr' => function (Strain $strain) {
-                    return ['data-clade' => $strain->getSpecies()->getClade()->getName()];
-                },
-                'multiple' => true,
-                'expanded' => true,
-                'constraints' => [
-                    new Count([
-                        'min' => 1,
-                    ]),
-                ],
             ])
         ;
     }
--- src/AppBundle/Form/Type/BlastType.php
+++ src/AppBundle/Form/Type/BlastType.php
@@ -2,9 +2,6 @@
 
 namespace AppBundle\Form\Type;
 
-use AppBundle\Entity\Strain;
-use Doctrine\ORM\EntityRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -16,7 +13,6 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use Symfony\Component\Validator\Constraints\Count;
 use Symfony\Component\Validator\Constraints\GreaterThan;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -48,54 +44,8 @@ class BlastType extends AbstractType
                     'tblastx' => 'tblastx',
                 ],
             ])
-            ->add('strainsFilter', EntityType::class, [
-                'class' => 'AppBundle\Entity\Clade',
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('clade')
-                        ->leftJoin('clade.species', 'species')
-                            ->addSelect('species')
-                        ->leftJoin('species.strains', 'strain')
-                            ->addSelect('strain')
-                        ->leftJoin('strain.authorizedUsers', 'authorizedUsers')
-                        ->orderBy('clade.name', 'asc')
-                        ->where('strain.public = true')
-                        ->orWhere('authorizedUsers = :user')
-                        ->setParameter('user', $this->tokenStorage->getToken()->getUser());
-                },
-                'choice_value' => 'name',
-                'choice_label' => 'name',
-                'placeholder' => 'All',
-                'required' => false,
+            ->add('strainsFilter', StrainsFilterType::class, [
                 'mapped' => false,
-            ])
-            ->add('strains', EntityType::class, [
-                'class' => 'AppBundle\Entity\Strain',
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('strain')
-                        ->leftJoin('strain.species', 'species')
-                            ->addSelect('species')
-                        ->leftJoin('species.clade', 'clade')
-                            ->addSelect('clade')
-                        ->leftJoin('strain.authorizedUsers', 'authorizedUsers')
-                        ->orderBy('species.scientificName', 'asc')
-                        ->addOrderBy('strain.name', 'asc')
-                        ->where('strain.public = true')
-                        ->orWhere('authorizedUsers = :user')
-                            ->setParameter('user', $this->tokenStorage->getToken()->getUser());
-                },
-                'choice_label' => function (Strain $strain) {
-                    return $strain->getSpecies()->getScientificName().' ('.$strain->getName().')';
-                },
-                'choice_attr' => function (Strain $strain) {
-                    return ['data-clade' => $strain->getSpecies()->getClade()->getName()];
-                },
-                'multiple' => true,
-                'expanded' => true,
-                'constraints' => [
-                    new Count([
-                        'min' => 1,
-                    ]),
-                ],
             ])
             ->add('filter', ChoiceType::class, [
                 'choices' => [
