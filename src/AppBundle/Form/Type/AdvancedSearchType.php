32dc94d28bd495d1c5192cb9b88169fbcc03e137
Mathieu Piot <mathieu.piot@inra.fr>
2017-09-06 14:53:33 +0200

    Fix strainsFilter in advancedSearch

--- src/AppBundle/Form/Type/AdvancedSearchType.php
+++ src/AppBundle/Form/Type/AdvancedSearchType.php
@@ -2,13 +2,25 @@
 
 namespace AppBundle\Form\Type;
 
+use AppBundle\Entity\Strain;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Validator\Constraints\Count;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class AdvancedSearchType extends AbstractType
 {
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -19,8 +31,54 @@ class AdvancedSearchType extends AbstractType
                     new NotBlank(),
                 ],
             ])
-            ->add('strainsFilter', StrainsFilterType::class, [
+            ->add('strainsFilter', EntityType::class, [
+                'class' => 'AppBundle\Entity\Clade',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('clade')
+                        ->leftJoin('clade.species', 'species')
+                        ->addSelect('species')
+                        ->leftJoin('species.strains', 'strain')
+                        ->addSelect('strain')
+                        ->leftJoin('strain.authorizedUsers', 'authorizedUsers')
+                        ->orderBy('clade.name', 'asc')
+                        ->where('strain.public = true')
+                        ->orWhere('authorizedUsers = :user')
+                        ->setParameter('user', $this->tokenStorage->getToken()->getUser());
+                },
+                'choice_value' => 'name',
+                'choice_label' => 'name',
+                'placeholder' => 'All',
+                'required' => false,
                 'mapped' => false,
+            ])
+            ->add('strains', EntityType::class, [
+                'class' => 'AppBundle\Entity\Strain',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('strain')
+                        ->leftJoin('strain.species', 'species')
+                        ->addSelect('species')
+                        ->leftJoin('species.clade', 'clade')
+                        ->addSelect('clade')
+                        ->leftJoin('strain.authorizedUsers', 'authorizedUsers')
+                        ->orderBy('species.scientificName', 'asc')
+                        ->addOrderBy('strain.name', 'asc')
+                        ->where('strain.public = true')
+                        ->orWhere('authorizedUsers = :user')
+                        ->setParameter('user', $this->tokenStorage->getToken()->getUser());
+                },
+                'choice_label' => function (Strain $strain) {
+                    return $strain->getSpecies()->getScientificName().' ('.$strain->getName().')';
+                },
+                'choice_attr' => function (Strain $strain) {
+                    return ['data-clade' => $strain->getSpecies()->getClade()->getName()];
+                },
+                'multiple' => true,
+                'expanded' => true,
+                'constraints' => [
+                    new Count([
+                        'min' => 1,
+                    ]),
+                ],
             ])
         ;
     }
