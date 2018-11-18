4aaccf70b4e1d567b2d67383b8710d907bca7a50
Mathieu Piot <math.piot@gmail.com>
2018-11-18 18:21:49 +0100

    Add Docker login action in scripts

--- docker/scripts/deploy-dev.sh
+++ docker/scripts/deploy-dev.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker login -u $DOCKER_HUB_LOGIN -p $DOCKER_HUB_PASSWORD
+
 docker tag $APP_DEV_IMAGE_NAME:$APP_DEV_IMAGE_TAG $APP_DEV_IMAGE_NAME:latest
 
 docker push $APP_DEV_IMAGE_NAME:$APP_DEV_IMAGE_TAG
--- docker/scripts/deploy-prod.sh
+++ docker/scripts/deploy-prod.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker login -u $DOCKER_HUB_LOGIN -p $DOCKER_HUB_PASSWORD
+
 docker pull $APP_PROD_IMAGE_NAME:latest
 
 docker build --target app-prod --cache-from=$APP_PROD_IMAGE_NAME:latest -t $APP_PROD_IMAGE_NAME:$TRAVIS_TAG .
